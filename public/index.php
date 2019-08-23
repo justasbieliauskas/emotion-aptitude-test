953f27a16aa2918db2e429af1d9818498e9236d9
Justas Bieliauskas <justyvas0@gmail.com>
2019-08-23 22:46:36 +0300

    Move assets & index.php to public/

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-$messages = include 'src/messages.php';
+$messages = include 'messages.php';
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
