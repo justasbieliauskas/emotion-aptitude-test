52ea2c460f052f027b8ca8691e94e814153060ed
Justas Bieliauskas <justyvas0@gmail.com>
2019-08-25 00:36:33 +0300

    Fetch messages from sqlite.

--- public/index.php
+++ public/index.php
@@ -8,4 +8,8 @@ use Twig\Loader\FilesystemLoader;
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
 $twig = new \Twig\Environment($loader);
 
-echo $twig->render('main.html.twig');
+$db = new PDO('sqlite:' . __DIR__ . '/../sqlite.db');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$messages = $db->query('SELECT * FROM messages');
+
+echo $twig->render('main.html.twig', ['messages' => $messages]);
